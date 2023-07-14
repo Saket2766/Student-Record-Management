67d7fe0da278b0e803cbdd6251be065b9210b26f
Saket2766 <84617740+Saket2766@users.noreply.github.com>
2023-07-14 17:46:57 +0530

    modified:   backend/app.js 	modified:   backend/package-lock.json 	modified:   backend/package.json 	new file:   frontend/.gitignore 	new file:   frontend/package-lock.json 	new file:   frontend/package.json 	new file:   frontend/public/index.html 	new file:   frontend/src/App.js 	new file:   frontend/src/index.css 	new file:   frontend/src/index.js

--- backend/app.js
+++ backend/app.js
@@ -3,27 +3,34 @@ require('dotenv').config();
 const express = require('express');
 const server = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
+
+//Set CORS Policy
+server.use(cors({
+    origin : "*"
+}))
 
 //Connect to DB and start server
-mongoose.connect(process.env.MONGO_URI).then(
-    () => {
-        server.listen(process.env.PORT, ()=>{
+mongoose.connect(process.env.MONGO_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    .then(() => {
+        server.listen(process.env.PORT, () => {
             console.log("Connected to DB. Server listening on port 4000")
         })
     }
-).catch( (err) => {
-    console.log(err);
-})
+    ).catch((err) => {
+        console.log(err);
+    })
 
 //Middleware
 
-server.use((req,res,next) =>{
-    console.log(req.method,req.path);
+server.use((req, res, next) => {
+    console.log(req.method, req.path);
     next();
 })
 
 
 //routes
-server.get('/', (req,res) => {
-    res.send({message : 'Response Sent'});
+server.get('/', (req, res) => {
+    res.send({ message: 'Response Sent' });
 })
