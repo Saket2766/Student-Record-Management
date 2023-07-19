a7832bbbc697d37e83074131dafd901b1d54dbfa
Saket2766 <84617740+Saket2766@users.noreply.github.com>
2023-07-19 21:49:59 +0530

    modified:   backend/Models/StudentModel.js

---
+++ backend/Models/StudentModel.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const subjectSchema = new Schema({
+    grade : {
+        type: "String",
+        required: true
+    },
+    marks : {
+        type: "Number",
+        required: true,
+        min: 0,
+        max: 100
+    }
+});
+
+const semseterSchema = new Schema({
+    subjects : [subjectSchema]
+});
+
+const studentSchema = new Schema({
+    username: {
+        type: "String",
+        required: true,
+        unique: true
+    },
+    name: {
+        type: "String",
+        required: true
+    },
+    roll: {
+        type: "String",
+        required: true,
+        unique: true
+    },
+    class: {
+        type: "String",
+        required: true
+    },
+    semesters: [semseterSchema]
+});
+
+module.exports = mongoose.model('Student',studentSchema);
