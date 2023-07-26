f63805a1f6ad616d90ec055919c8ee76f3a3e6de
Saket2766 <84617740+Saket2766@users.noreply.github.com>
2023-07-26 21:38:54 +0530

    modified:   frontend/src/components/CreateUser.js 	modified:   frontend/src/styles/adminDashboard.css 	modified:   frontend/src/styles/home.css

--- frontend/src/components/CreateUser.js
+++ frontend/src/components/CreateUser.js
@@ -8,16 +8,29 @@ const CreateUser = () => {
         <form onSubmit={handleSubmit}>
             <h1>Create New User</h1>
             <div className="flex-form">
-                <label>User Type :</label>
+                <label>User Type</label>
                 <select required>
                     <option value="select">Select User Type</option>
                     <option value="teacher">Teacher</option>
                     <option value="student">Student</option>
                 </select>
             </div>
-            <div className="flex-form">
-                <label>Password:</label>
-                <input type="password"/>
+
+            <label>Username</label>
+            <input type="text"/>
+
+            <label>Password</label>
+            <input type="password"/>
+
+            <div className="student">
+                <label>Name</label>
+                <input type="text"/>
+
+                <label>Roll No</label>
+                <input type="text"/>
+
+                <label>Class</label>
+                <input type="text"/>
             </div>
         </form>
      );
